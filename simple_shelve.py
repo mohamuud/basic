c1024f5748d840e64d29cec55c67a79fa1a49fcd
mohamuud mohamed <mohamuudmohamed1@Gmail.com>
2018-07-08 18:10:24 +0300

    Update simple_shelve.py

--- simple_shelve.py
+++ simple_shelve.py
@@ -5,11 +5,11 @@ ch = int(input("Enter the option 1 for Entry or 0 for retrive \n "))
 inc = 0
 if ch == 1:
     p = int(input("How many persons you went to register \n "))
-    with shelve.open("camon") as fruit:
+    with shelve.open("camon") as person:
         while p > inc:
             name = input("enter The name of the student")
             desc = input("Enter the discription of the member")
-            fruit[name] = desc
+            person[name] = desc
             inc += 1
 else:
     with shelve.open("camon") as read:
