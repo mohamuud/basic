c9c317e41df17cede47a964e2387ab86cebbaf1f
mohamuud mohamed <mohamuudmohamed1@Gmail.com>
2018-07-22 14:23:13 +0300

    Update input_logger.py

--- input_logger.py
+++ input_logger.py
@@ -1,18 +1,21 @@
 # this program will log your input
 # you can use sync method to reduce memory Usage but this is simple one
+
 import time
+import datetime
+import random
 import shelve
 times = time.localtime()
 print("*"*20 + " welcome " + "*"*20)
 print("""Enter you option
-1.Save 
+1.Save
 2.read""")
 ch = int(input())
 if ch == 1:
     file =  shelve.open("files")
     print("Enter your notes")
     note = input()
-    date = " sanadka " + str(times.tm_year) + " - bisha " +str(times.tm_mon) + " - malinaa " +str(times.tm_mday) + " - daqiiqada " + str(times.tm_min) + " - mirirada " + str(times.tm_sec)
+    date = str(datetime.datetime.now())
     file[date] = note
     file.close()
 else:
